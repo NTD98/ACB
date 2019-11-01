709f31a79c745fee02e9354a254b59e5664e33e6
NTD98 <37630367+NTD98@users.noreply.github.com>
2019-11-01 16:27:59 +0700

    mock api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './module/login/login.component';
@@ -8,6 +8,7 @@ import { SearchInformationComponent } from './searchinformation/searchinformatio
 import { HeaderComponent } from './_template/header/header.component';
 import { FooterComponent } from './_template/footer/footer.component';
 
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { LoginService } from '../app/module/login/login.service';
 import { HomeComponent } from './module/home/home.component';
@@ -24,7 +25,9 @@ import { HomeComponent } from './module/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     LoginService,
--- src/app/module/login/login.component.ts
+++ src/app/module/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 import {LoginService} from './login.service';
-
+import {AuthenticationService} from '../../_services/authentication.service';
 
 @Component({
   selector: 'login',
@@ -8,9 +9,21 @@ import {LoginService} from './login.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor(private loginService: LoginService) { }
+  username:string
+  password:string
+  constructor(private loginService: LoginService,
+    private authen: AuthenticationService,
+    private router : Router) { }
   login(){
+    console.log("usertest",this.username)
+    this.authen.login(this.username,this.password).pipe()
+    .subscribe(
+        data => {
+            this.router.navigate(['./home']);
+        },
+        error => {
+            console.log(error)
+        });
     this.loginService.toggle();
   }
   ngOnInit() {
